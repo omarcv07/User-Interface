7840deb8da7151b7ca0eef412ae6695177abe303
omarcv07 <omarenriquect@gmail.com>
2020-07-05 23:33:41 -0400

    Gallery, aboutus, interests, journal, curious and footer sections

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
+// carousel 1 
 $(document).ready(function() {
+    $('#carouselFunction').carousel({ interval: 5000 });
+    $('[data-toggle="tooltip"]').tooltip();
+});
 
-$('#carouselFunction').carousel({ interval: 1000 });
+// Carousel 2 
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
 });
